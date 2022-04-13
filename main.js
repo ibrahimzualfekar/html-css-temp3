584a026708d143f3a2c24248676f9652bcf3dee0
biko710 <ibrahim.abdelmonem710@gmail.com>
2022-04-13 12:33:36 +0200

    js progress effect

--- main.js
+++ main.js
@@ -38,3 +38,19 @@ let counter = setInterval(() => {
 }, 1000);
 
 
+//..........................................................................................................
+
+let section = document.querySelector("#our-skills");
+let spans = document.querySelectorAll(".the-progress span");
+
+
+
+window.onscroll = function (){
+    if( window.scrollY >= section.offsetTop - 350 ){
+        console.log("R S T");
+        spans.forEach((span) =>{
+            span.style.width = span.dataset.width;
+        })
+
+    }
+};
