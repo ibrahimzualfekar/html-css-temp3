2eb1b913187a22fdd4e63cfd81e928a4f84731c1
biko710 <ibrahim.abdelmonem710@gmail.com>
2022-04-19 18:03:13 +0200

    add a dynamic numbers on scrolling

--- main.js
+++ main.js
@@ -54,3 +54,39 @@ window.onscroll = function (){
 
     }
 };
+
+
+// ................................................................................
+
+
+let nums = document.querySelectorAll('.box .number');
+let Section = document.querySelector(".stats");
+let started = false;
+
+
+
+window.onscroll = function () {
+    if (window.scrollY >= Section.offsetTop) {
+        if(!started){
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+
+
+function startCount(el){
+    let goal= el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+
+        if(el.textContent == goal){
+            clearInterval(count)
+        }
+
+    }, 2000 / goal);
+};
+
+// ............................................................................
+
